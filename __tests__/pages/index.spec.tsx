d27d9dfa2fb9853fb041f03f5c3c35e85d9199ed
Jarrku <simonvdbroeck@gmail.com>
2018-03-04 19:39:15 +0100

    Update tests to new API

--- __tests__/pages/index.spec.tsx
+++ __tests__/pages/index.spec.tsx
@@ -1,13 +1,9 @@
+import { shallow } from "enzyme";
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-import * as TestUtils from "react-dom/test-utils";
 
 import Index from "../../pages/index";
 
-it("CheckboxWithLabel changes the text after click", () => {
-  // Render a checkbox with label in the document
-  const checkbox = TestUtils.renderIntoDocument(<Index />);
-
-  const checkboxNode = ReactDOM.findDOMNode(checkbox);
-  expect(checkboxNode).toBeDefined();
+it("Index File", () => {
+  const index = shallow(<Index />);
+  expect(index).toBeDefined();
 });
--- pages/index.spec.tsx
+++ pages/index.spec.tsx
@@ -1,13 +1,9 @@
+import { shallow } from "enzyme";
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-import * as TestUtils from "react-dom/test-utils";
 
 import Index from "./index";
 
 it("Index File", () => {
-  // Render a checkbox with label in the document
-  const checkbox = TestUtils.renderIntoDocument(<Index />);
-
-  const checkboxNode = ReactDOM.findDOMNode(checkbox);
-  expect(checkboxNode).toBeDefined();
+  const index = shallow(<Index />);
+  expect(index).toBeDefined();
 });
