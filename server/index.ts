adee562034ae82b2acca2866f0694ee3c74aacc2
Benjamin van Zwienen <benjaminvanzwienen@live.nl>
2018-03-06 10:34:04 +0100

    Setup/graphql backend (#18)
    * Change server to use Express
    
    * Create basic GraphQL backend
    
    * Add ExerciseImage to GraphQL
    
    * Change resolvers from promise to async
    
    * Merge exerciseimage into exercise resolver
    
    * Dependency bump + Yarn Lock update
    
    * Add additional Typescript Types
    
    * Add .prettierrc to better comply with TSLint rules
    
    * Add helper method to make API requests easier
    
    * Change export and import of schema.ts

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,8 @@
-import { createServer } from "http";
+import { graphiqlExpress, graphqlExpress } from "apollo-server-express";
+import * as bodyParser from "body-parser";
+import * as express from "express";
 import * as next from "next";
-import { parse } from "url";
+import schema from "./schema";
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
@@ -8,10 +10,28 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-  }).listen(port, err => {
+  const server = express();
+
+  server.use(
+    "/graphql",
+    bodyParser.json(),
+    graphqlExpress({
+      schema,
+    }),
+  );
+
+  server.use(
+    "/graphiql",
+    graphiqlExpress({
+      endpointURL: "/graphql",
+    }),
+  );
+
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, err => {
     if (err) throw err;
     // tslint:disable-next-line
     console.log(`> Ready on http://localhost:${port}`);
---
+++ server/schema.ts
@@ -0,0 +1,89 @@
+import { makeExecutableSchema } from "graphql-tools";
+import * as fetch from "isomorphic-unfetch";
+
+// Helper method to fetch JSON
+const getJSON = async (url: string) => {
+  const res = await fetch(url);
+  return res.json();
+};
+
+// Types
+const typeDefs = `
+  type Query {
+    categories: [Category]
+    exercises(category: Int!) : [Exercise]
+    exercise(id: Int!) : Exercise
+    exerciseimage(id: Int!) : [ExerciseImage]
+  }
+
+  type Category {
+    id: Int!
+    name: String!
+  }
+
+  type Exercise {
+    id: Int!
+    license_author: String
+    status: String
+    description: String
+    name: String
+    name_original: String
+    creation_data: String
+    uuid: String
+    license: Int
+    category: Int
+    language: Int
+    muscles: [Int]
+    muscles_secondary: [Int]
+    equipment: [Int]
+    image: [ExerciseImage]
+  }
+
+  type ExerciseImage {
+    id: Int!
+    license_author: String
+    status: String
+    image: String
+    is_main: Boolean
+    license: Int
+    exercise: Int!
+  }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    categories: async () => {
+      const { results } = await getJSON(
+        "https://wger.de/api/v2/exercisecategory",
+      );
+
+      return results;
+    },
+    exercise: async (_, { id }) => {
+      const info = getJSON(`https://wger.de/api/v2/exercise/${id}`);
+      const image = getJSON(
+        `https://wger.de/api/v2/exerciseimage?exercise=${id}`,
+      );
+      const [dataInfo, dataImage] = [await info, await image];
+
+      return {
+        ...dataInfo,
+        image: dataImage.results,
+      };
+    },
+    exercises: async (_, { category }) => {
+      const { results } = await getJSON(
+        `https://wger.de/api/v2/exercise/?category=${category}&language=2&equipment=7`,
+      );
+
+      return results;
+    },
+  },
+};
+
+// Export schema
+export default makeExecutableSchema({
+  resolvers,
+  typeDefs,
+});
