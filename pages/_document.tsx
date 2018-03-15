21b80b9829aa5e11b57ed4781ff8212ab9c02c51
Benjamin van Zwienen <benjaminvanzwienen@live.nl>
2018-03-15 09:29:21 +0100

    Add Bootstrap 4 CSS

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,12 @@ export default class MyDocument extends Document {
         <Head>
           <title>My Page</title>
           {this.props.styleTags}
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+            crossorigin="anonymous"
+          />
         </Head>
         <body>
           <Main />
